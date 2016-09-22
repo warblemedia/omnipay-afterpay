f17a4bf6e3798e5190e6aea109494834c60a68c4
Jeremy Worboys <jw@jeremyworboys.com>
2016-09-22 16:27:24 +1000

    Add complete request class

---
+++ src/Message/CompletePurchaseRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Omnipay\AfterPay\Message;
+
+class CompletePurchaseRequest extends AbstractRequest
+{
+    /**
+     * Get the raw data array for this message. The format of this varies from gateway to
+     * gateway, but will usually be either an associative array, or a SimpleXMLElement.
+     *
+     * @return mixed
+     */
+    public function getData()
+    {
+        // TODO: Implement getData() method.
+    }
+}
