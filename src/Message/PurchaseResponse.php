d5996759a4cdf192285223c5f0260698e91d5401
Jeremy Worboys <jw@jeremyworboys.com>
2016-09-22 15:27:34 +1000

    Add get token to response

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -4,4 +4,11 @@ namespace Omnipay\AfterPay\Message;
 
 class PurchaseResponse extends Response
 {
+    /**
+     * @return string|null
+     */
+    public function getToken()
+    {
+        return isset($this->data['token']) ? $this->data['token'] : null;
+    }
 }
